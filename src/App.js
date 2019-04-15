31d2115baeeb3f306a1ad95e26c2097554420e11
Duc Vu <de.yd315@gmail.com>
2019-04-15 12:11:23 -0500

    skeleton app created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, NavLink, Route  } from 'react-router-dom';
+
+
 import './App.css';
+
+const Stories = props => (
+  <div>Stories</div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          
+          <Route path='/' component={Stories} />
+        </div>
+      </Router>
     );
   }
 }
