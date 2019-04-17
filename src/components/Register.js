a90d371385fc882cdc2a3119f46de11892637ed2
Duc Vu <de.yd315@gmail.com>
2019-04-17 07:34:11 -0500

    sign up form added

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,17 +1,86 @@
 import React from 'react'
 
-const Register = props => {
-    console.log("hi")
-    return (
-    <div>
-        <h1>Let's Create Your Profile!</h1>
-        <form>
-            <input type='text' />
-            <input type='password'/>
-            <button>Done</button>
-        </form>
-    </div>)
+class Register extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+        }
+    }
+
+    editRegisterForm = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    register = event => {
+
+    }
+
+    render = () => {
+        return (
+        <div>
+            <h1>Let's Create Your Profile!</h1>
+            <form>
+                
+                <input 
+                    type='text'
+                    placeholder='First Name' 
+                    required
+                    value={this.state.firstName}
+                    onChange={this.editRegisterForm}
+                    name='firstName'
+                />
+
+                
+                <input 
+                    type='text'
+                    placeholder='Last Name' 
+                    required
+                    value={this.state.lastName}
+                    onChange={this.editRegisterForm}
+                    name='lastName'
+                />
+
+                <input 
+                    type='text'
+                    placeholder='Username'
+                    minLength='3' 
+                    required
+                    value={this.state.username}
+                    onChange={this.editRegisterForm}
+                    name='username'
+                />            
+
+                <input 
+                    type='email'
+                    placeholder='Email' 
+                    required
+                    value={this.state.email}
+                    onChange={this.editRegisterForm}
+                    name='email'
+                />             
+    
+                <input 
+                    minLength='6'
+                    type='password'
+                    placeholder='Password' 
+                    required
+                    value={this.state.password}
+                    onChange={this.editRegisterForm}
+                    name='password'
+                />
+                <button onClick={this.register}>Done</button>
+            </form>
+        </div>)
+    }
 }
+
 
 export default Register
 
